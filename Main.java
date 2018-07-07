4c5c549cd929af8d7412b5da4acce82d3e2b2799
luyy1234 <2462051937@qq.com>
2018-07-07 10:39:25 +0800

    语法练习

---
+++ Main.java
@@ -0,0 +1,39 @@
+package session1;
+
+import java.awt.List;
+import java.util.ArrayList;
+
+/*
+ * java基础语法操作练习
+ */
+public class javaBase {
+	public static void main(String[] args){
+		//print(1, "123");
+//		String str="Hello,World";
+//		print(1, str.compareTo("Hello,World"));//比较第一个不相等的字符的ASCII码之差
+//		print(2, str.compareTo("Hello,Vorld"));
+//		print(3, str.compareTo("Hello,Xorld"));
+//		print(0, str.charAt(4));//指定位置的字符
+//		print(4, str.replace('l', 'v'));//替换
+//		print(5, str.replaceAll("Hello", "Hi"));
+//		print(6, str.concat("!!"));//追加
+//		print(7, str.codePointAt(1));//ASCII码
+//		print(8, str.toLowerCase());//大小写转换
+		ArrayList<String> lstStr= new ArrayList<String>();
+	}
+	public static void print(int i,Object obj){
+		System.out.println(String.format("{%d},%s",i,obj.toString()));
+	}
+	public void demoList(){
+		
+	}
+	public void demoSet(){
+		
+	}
+	public void demoMap(){
+		
+	}
+	public void demoException(){
+		
+	}
+}
